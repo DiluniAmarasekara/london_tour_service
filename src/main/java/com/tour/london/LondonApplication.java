3154afa76748cb1619464fde8cd1ab0c632459b0
DiluniAmarasekara <dilunia6@gmail.com>
2022-12-07 19:49:10 +0530

    initial commit

---
+++ src/main/java/com/tour/london/LondonApplication.java
@@ -0,0 +1,13 @@
+package com.tour.london;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LondonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LondonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tour/london/LondonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tour.london;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LondonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
